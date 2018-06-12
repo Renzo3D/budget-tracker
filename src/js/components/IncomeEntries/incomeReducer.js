c4ed0b1e47fbadeec9e25d5ba24da1bc49be3b55
RenzoPozo <renzopozo@yahoo.com>
2018-06-12 15:26:02 -0700

    budget-tracker

---
+++ src/js/components/IncomeEntries/incomeReducer.js
@@ -0,0 +1,43 @@
+const defaultState = {
+  description: '',
+  amount: '',
+  lineItems: []
+};
+
+export default function IncomeReducer(state = defaultState, action) {
+  
+  const { type, payload } = action;
+
+  switch (type) {
+      case 'UPDATE_INCOME_DESCRIPTION': {
+          return {
+              ...state,
+              description: payload.description
+          }
+      }
+      case 'UPDATE_INCOME_AMOUNT': {
+          return {
+              ...state,
+              amount: payload.amount
+          }
+      }
+      case 'ADD_INCOME': {
+          const { description, amount } = action.payload;
+          return {
+              description: '',
+              action: '',
+              lineItems: [
+                  ...state.lineItems,
+                  { description, amount }
+              ]
+          };
+      }
+
+      default: {
+          return state;
+      }
+
+  }
+  // the `state = defaultState` above is new ES6 syntax
+  // for defining a default value on a parameter
+}
